f0024b9a521a2b9349142718821b5f95bdb57e93
Canaan Etaigbenu <kestylove5@gmail.com>
2016-04-19 19:42:02 +0000

    travis

---
+++ src/Innovating/Exceptions/ErrorHandlerService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Canan Etaigbenu
+ * User: canaan5
+ * Date: 4/17/16
+ * Time: 3:58 AM
+ */
+
+namespace Innovating\Exceptions;
+
+
+class ErrorHandlerService
+{
+
+}
---
+++ src/Innovating/Log/LogHandler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Canan Etaigbenu
+ * User: canaan5
+ * Date: 4/17/16
+ * Time: 3:32 AM
+ */
+
+namespace Innovating\Log;
+
+
+class LogHandler
+{
+
+}
